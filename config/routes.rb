cd3b1e6250a32ecb4cfd3ea82d9b2a66abbd481d
Ajay Barot <ajubarot@gmail.com>
2020-04-11 18:30:56 +0530

    1. Created patient devise 2. Created doctor devise 3. Override default devise controller 4. Welcome page if current resource is available

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :patients, controllers: {
+    sessions: 'patients/sessions',
+    registrations: 'patients/registrations'
+  }
+  devise_for :doctors, controllers: {
+    sessions: 'doctors/sessions',
+    registrations: 'doctors/registrations'
+  }
   root 'welcome#index'
 end
